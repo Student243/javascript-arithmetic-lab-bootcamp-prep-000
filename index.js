230443d1b3cd5a37143223f0238fb82011729e05
Kevin Andrew Johnson <kjohnson5315@hotmail.com>
2018-05-27 20:52:00 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,31 @@
+var a,b
+a = Math.floor(Math.random() * 1000)
+b = Math.floor(Math.random() * 1000)
+
+function add(a,b) {
+  return a+b
+}
+function subtract(a,b) {
+  return a-b
+}
+function multiply(a,b) {
+  return a*b
+} 
+function divide(a,b) {
+  return a/b
+}
+function inc(a) {
+  return a+1
+}
+function dec(a) {
+  return a-1
+}
+function makeInt(a,string) {
+  return a
+}
+function makeInt(n) { 
+  return parseInt(n,10) 
+}
+function makeInt(isNaN) {
+  return NaN 
+}
